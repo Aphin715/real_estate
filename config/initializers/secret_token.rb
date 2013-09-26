d481a0d026ba3b2303ceefd8f9a66c66766cb54f
Alex Phinizy <AlexPhinizy@gmail.com>
2013-09-26 12:45:48 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PhinAssessment::Application.config.secret_key_base = '1a5488fa9e3116ce6ee105a734794b22587e0c1e25ec72dbab138239bffc8c7bee3dcf4e7de3f15abe1b21b270801f22c387263facb03a08aabb125131237291'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PhinAssessment::Application.config.session_store :cookie_store, key: '_phin_assessment_session'
